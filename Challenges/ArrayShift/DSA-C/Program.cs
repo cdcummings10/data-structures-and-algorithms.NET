69bd814d4f35af778c868a0b4dfd487488b30f01
cdcummings10 <ccummings0913@gmail.com>
2019-10-17 10:30:01 -0700

    Trying to fix git hub commits

---
+++ Challenges/ArrayShift/DSA-C/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_C
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] InsertShiftArray(int[] arr, int num)
+        {
+            int initialLength = arr.Length;
+            int middleIndex = 0;
+
+            if (initialLength % 2 == 0)
+            {
+                middleIndex = (initialLength / 2);
+            }
+            else
+            {
+                middleIndex = (initialLength / 2) + 1;
+            }
+
+            int[] answer = new int[initialLength + 1];
+            bool inserted = false;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (middleIndex == i)
+                {
+                    answer[i] = num;
+                    inserted = true;
+                }
+                else if (inserted)
+                {
+                    answer[i] = arr[i - 1];
+                }
+                else
+                {
+                    answer[i] = arr[i];
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ Challenges/ArrayShift/README.md
@@ -0,0 +1,37 @@
+# .NET Data Structures and Algorithms
+
+## Insert Shift Arrays
+
+*Author: Chris Cummings*
+
+---
+
+## Description
+
+Write a function called insertShiftArray which takes in an array and the value to be added. 
+Without utilizing any of the built-in methods available to your language, 
+return an array with the new value added at the middle index.
+
+---
+
+##Approach
+
+In order to calculate the middle of the array under the assumption that I needed to ceiling my division,
+I wrote an if statement for odd or even lengths that adds 1 to the middle index if its odd.  
+I created a boolean to tell if an object was inserted in order to use in my for loop so after 
+the halfway mark, the value would be using the original array index -1 to account for an extra 
+value being added to the array.
+
+---
+
+## Visuals
+![Code Challenge 2 Whiteboard](../../assets/arrayShift.jpg)
+
+---
+
+## Change Log
+
+1.1: Finished Code Challenge 2 - 10/17/2019
+
+---
+
