a9915622cffccbdb37db1d362e7d316dae0f049c
cdcummings10 <ccummings0913@gmail.com>
2019-10-17 09:55:51 -0700

    Finished code challenge 2

--- DSA-C/Program.cs
+++ DSA-C/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace DSA_C
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+        }
+
+        public static int[] InsertShiftArray(int[] arr, int num)
+        {
+            int initialLength = 0;
+            int middleIndex = 0;
+
+            if (arr.Length > 0)
+            {
+                initialLength = arr.Length;
+            }
+
+            if (initialLength % 2 == 0)
+            {
+                middleIndex = (initialLength / 2);
+            }
+            else
+            {
+                middleIndex = (initialLength / 2) + 1;
+            }
+
+            int[] answer = new int[initialLength + 1];
+            bool inserted = false;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (middleIndex == i)
+                {
+                    answer[i] = num;
+                    inserted = true;
+                }
+                else if (inserted)
+                {
+                    answer[i] = arr[i - 1];
+                }
+                else
+                {
+                    answer[i] = arr[i];
+                }
+            }
+            return answer;
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,27 @@
+# .NET Data Structures and Algorithms
+
+## Insert Shift Arrays
+
+*Author: Chris Cummings*
+
+---
+
+## Description
+
+Write a function called insertShiftArray which takes in an array and the value to be added. 
+Without utilizing any of the built-in methods available to your language, 
+return an array with the new value added at the middle index.
+
+---
+
+## Visuals
+![Code Challenge 2 Whiteboard](imgs/codechallenge2)
+
+---
+
+## Change Log
+
+1.1: Finished Code Challenge 2 - 10/17/2019
+
+---
+
