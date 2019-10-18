5fd44c023e891496462c7babc91abe2273b71922
cdcummings10 <ccummings0913@gmail.com>
2019-10-18 11:16:45 -0700

    Updated projects to core instead of framework.

--- Challenges/ArrayShift/XUnitTestProject1/UnitTest1.cs
+++ Challenges/ArrayShift/XUnitTestProject1/UnitTest1.cs
@@ -1,6 +1,6 @@
 using System;
 using Xunit;
-using static DSA_C.Program;
+using static ArrayShift.Program;
 
 namespace XUnitTestProject1
 {
@@ -8,7 +8,7 @@ namespace XUnitTestProject1
     {
         [Theory]
         [InlineData(new int[] { 2, 4, 6, 8 }, 5, new int[] { 2, 4, 5, 6, 8 })]
-        [InlineData(new int[] {4, 8, 15, 23, 42}, 16, new int[] { 4, 8, 15, 16, 23, 42 })]
+        [InlineData(new int[] { 4, 8, 15, 23, 42 }, 16, new int[] { 4, 8, 15, 16, 23, 42 })]
         public void Challenge2TestIfCorrectOutputOfArrayWithInsertedValue(int[] arr, int num, int[] answer)
         {
             Assert.Equal(answer, InsertShiftArray(arr, num));
@@ -25,7 +25,7 @@ namespace XUnitTestProject1
         [Fact]
         public void Challenge2TestIncorrectOutput()
         {
-            int[] param = new int[] { 2, 15,17, 90, 223, 1 };
+            int[] param = new int[] { 2, 15, 17, 90, 223, 1 };
             int[] answer = new int[] { 2, 15, 17, 90, 223, 1, 3 };
             Assert.NotEqual(answer, InsertShiftArray(param, 3));
         }
