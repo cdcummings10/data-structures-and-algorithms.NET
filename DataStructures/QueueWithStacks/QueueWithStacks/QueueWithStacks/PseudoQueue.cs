5f187ea37cecddd3f4b4ad0129455107dcbfdcf8
cdcummings10 <ccummings0913@gmail.com>
2019-10-28 13:46:17 -0700

    Finished initial coding

---
+++ DataStructures/QueueWithStacks/QueueWithStacks/QueueWithStacks/PseudoQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueWithStacks
+{
+    public class PseudoQueue
+    {
+        private Stack<int> storageStack = new Stack<int>();
+        private Stack<int> answerStack = new Stack<int>();
+        public int Front { get; set; }
+        public int Rear { get; set; }
+
+
+        public void Enqueue(int num)
+        {
+            if (storageStack.Count == 0)
+            {
+                Front = num;
+            }
+            storageStack.Push(num);
+            Rear = num;
+        }
+        public int Dequeue()
+        {
+            if (storageStack.Count != 0)
+            {
+                foreach (int num in storageStack)
+                {
+                    answerStack.Push(storageStack.Pop());
+                }
+                int answer = answerStack.Pop();
+                foreach (int num in answerStack)
+                {
+                    storageStack.Push(answerStack.Pop());
+                }
+                return answer;
+            }
+            else { return -1; }
+        }
+    }
+}
---
+++ DataStructures/QueueWithStacks/QueueWithStacks/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+using QueueWithStacks;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        public PseudoQueue testQueue = new PseudoQueue();
+        [Fact]
+        public void TestIfYouCanEnqueueAValue()
+        {
+            testQueue.Enqueue(1);
+            testQueue.Enqueue(4);
+            testQueue.Enqueue(12);
+            testQueue.Enqueue(15);
+            testQueue.Enqueue(44);
+            testQueue.Enqueue(9);
+
+            Assert.Equal(9, testQueue.Rear);
+        }
+        [Fact]
+        public void TestifYouCanDequeueAnItemAndItsTheLastItemAdded()
+        {
+            testQueue.Enqueue(1);
+            testQueue.Enqueue(4);
+            testQueue.Enqueue(12);
+            testQueue.Enqueue(15);
+            testQueue.Enqueue(44);
+            testQueue.Enqueue(9);
+
+            Assert.Equal(9, testQueue.Dequeue());
+        }
+    }
+}
