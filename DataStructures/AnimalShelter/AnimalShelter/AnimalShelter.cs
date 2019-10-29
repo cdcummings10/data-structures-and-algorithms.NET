0909e13c404cf7f8573a803a64f8ae662677f350
cdcummings10 <ccummings0913@gmail.com>
2019-10-29 13:40:35 -0700

    Finished inital coding before testing.

---
+++ DataStructures/AnimalShelter/AnimalShelter/AnimalShelter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalShelter
+{
+    class AnimalShelter
+    {
+        private Stack<Animal> storageStack = new Stack<Animal>();
+        private Stack<Animal> answerStack = new Stack<Animal>();
+
+        public void Enqueue(Animal animal)
+        {
+            storageStack.Push(animal);
+        }
+
+        public Animal Dequeue(string pref)
+        {
+            Animal answer = null;
+            int count = storageStack.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                answerStack.Push(storageStack.Pop());
+            }
+            for (int i = 0; i < count; i++)
+            {
+                Animal current = answerStack.Pop();
+                if (answer == null)
+                {
+                    switch (pref)
+                    {
+                        case "dog":
+                            if (current is Dog)
+                            {
+                                answer = current;
+                            }
+                            else
+                            {
+                                storageStack.Push(current);
+                            }
+                            break;
+                        case "cat":
+                            if (current is Cat)
+                            {
+                                answer = current;
+                            }
+                            else
+                            {
+                                storageStack.Push(current);
+                            }
+                            break;
+                        default:
+                            storageStack.Push(current);
+                            break;
+                    }
+                }
+                else
+                {
+                    storageStack.Push(current);
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ DataStructures/AnimalShelter/AnimalShelter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AnimalShelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog();
+            Dog dog2 = new Dog();
+            Dog dog3 = new Dog();
+            Cat cat1 = new Cat();
+            Cat cat2 = new Cat();
+            Cat cat3 = new Cat();
+
+            AnimalShelter animalShelter = new AnimalShelter();
+
+            animalShelter.Enqueue(dog1);
+            animalShelter.Enqueue(dog2);
+            animalShelter.Enqueue(cat1);
+            animalShelter.Enqueue(dog3);
+            animalShelter.Enqueue(cat2);
+            animalShelter.Enqueue(cat3);
+
+            Animal answer = animalShelter.Dequeue("cat");
+
+
+        }
+    }
+}
