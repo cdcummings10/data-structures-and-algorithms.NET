da5a6e35a657d8c0df6ad1a891d7678169ca05cd
cdcummings10 <ccummings0913@gmail.com>
2019-11-18 13:38:22 -0800

    Finished blog for insertion sort.

---
+++ DataStructures/Trees/Trees/BinaryTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    class BinaryTree : Tree<int>
+    {
+
+        public void Add(int value)
+        {
+            Node<int> newNode = new Node<int>(value);
+            Node<int> node = Root;
+            if(Root == null)
+            {
+                Root = newNode;
+            }
+            else
+            {
+            Add(node, newNode);
+            }
+        }
+        private void Add(Node<int> node, Node<int> newNode)
+        {
+            if (newNode.Value >= node.Value && node.RightChild != null)
+            {
+                Add(node.RightChild, newNode);
+            }
+            else if (newNode.Value <= node.Value && node.LeftChild != null)
+            {
+                Add(node.LeftChild, newNode);
+            }
+            else
+            {
+                if (newNode.Value >= node.Value)
+                {
+                    node.RightChild = newNode;
+                }
+                else
+                {
+                    node.LeftChild = newNode;
+                }
+            }
+        }
+        public bool Contains(int value)
+        {
+            bool answer = false;
+            if (Root == null)
+            {
+                return answer;
+            }
+            else
+            {
+                answer = Contains(Root, value);
+            }
+            return answer;
+        }
+        private bool Contains(Node<int> node, int value)
+        {
+            if (value > node.Value && node.RightChild != null)
+            {
+                Contains(node.RightChild, value);
+            }
+            else if (value < node.Value && node.LeftChild != null)
+            {
+                Contains(node.LeftChild, value);
+            }
+            else if (value == node.Value)
+            {
+                return true;
+            }
+                return false;
+        }
+    }
+}
---
+++ DataStructures/Trees/Trees/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> LeftChild { get; set; } = null;
+        public Node<T> RightChild { get; set; } = null;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+}
