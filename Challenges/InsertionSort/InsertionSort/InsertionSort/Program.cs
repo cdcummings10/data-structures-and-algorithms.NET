02c5718b309eeeb63fdbdcc7c4ab218f7bc71c90
cdcummings10 <ccummings0913@gmail.com>
2019-11-18 13:50:12 -0800

    Finished coding part of insertionsort challenge

---
+++ Challenges/InsertionSort/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(',', InsertionSort(new int[] { 20, 18, 12, 8, 5, -2})));
+            Console.WriteLine(String.Join(',', InsertionSort(new int[] { 5, 12, 7, 5, 5, 7 })));
+            Console.WriteLine(String.Join(',', InsertionSort(new int[] { 2, 3, 5, 7, 13, 11 })));
+        }
+        static int[] InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int temp = arr[i];
+                while(j >= 0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                    arr[j + 1] = temp;
+                }
+            }
+            return arr;
+        }
+    }
+}
