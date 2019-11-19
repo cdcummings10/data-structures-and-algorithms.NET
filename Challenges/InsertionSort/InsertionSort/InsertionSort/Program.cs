f121d751b14796adb9205560b5528a16a37d536c
cdcummings10 <ccummings0913@gmail.com>
2019-11-19 08:16:39 -0800

    Finished Unit Tests

--- Challenges/InsertionSort/InsertionSort/InsertionSort/Program.cs
+++ Challenges/InsertionSort/InsertionSort/InsertionSort/Program.cs
@@ -2,15 +2,20 @@
 
 namespace InsertionSort
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(String.Join(',', InsertionSort(new int[] { 20, 18, 12, 8, 5, -2})));
-            Console.WriteLine(String.Join(',', InsertionSort(new int[] { 5, 12, 7, 5, 5, 7 })));
-            Console.WriteLine(String.Join(',', InsertionSort(new int[] { 2, 3, 5, 7, 13, 11 })));
+            Console.WriteLine(String.Join(',', InsertionSorter(new int[] { 20, 18, 12, 8, 5, -2})));
+            Console.WriteLine(String.Join(',', InsertionSorter(new int[] { 5, 12, 7, 5, 5, 7 })));
+            Console.WriteLine(String.Join(',', InsertionSorter(new int[] { 2, 3, 5, 7, 13, 11 })));
         }
-        static int[] InsertionSort(int[] arr)
+        /// <summary>
+        /// Sorts an array of integers from smallest to largest using the "insertion sort" algorithm.
+        /// </summary>
+        /// <param name="arr">Takes in an array of integers.</param>
+        /// <returns>Returns the same array of integers as the parameter, but now sorted.</returns>
+        public static int[] InsertionSorter(int[] arr)
         {
             for (int i = 1; i < arr.Length; i++)
             {
---
+++ Challenges/InsertionSort/README.md
@@ -0,0 +1,40 @@
+# .NET Data Structures and Algorithms
+
+## Insertion Sort
+
+*Author: Chris Cummings*
+
+---
+
+## Description
+
+Write a method called InsertionSort that takes in an array of integers and
+returns the array sorted from least to greatest.
+
+---
+
+## Approach
+
+I first created a for loop for the array, and inside I defined a j variable
+and a temp variable.  The j variable represents everything to the left of
+array[j] as being already sorted. The temp variable is a variable I can use
+inside of a while loop to keep track of the current number being evaluated.
+
+I then created a while loop that ran while j >= 0 (the end of the array)
+and while the temp variable was less than array[j]. In this loop I stepped
+through the the array, replacing the array[j+1] with array[j], and setting
+j to j-1.
+
+---
+
+## Blog
+[Link to Blog](../../assets/binarySearch.jpg)
+
+---
+
+## Change Log
+
+1.1: Finished Insertion Sort Challenge - 11/18/2019
+
+---
+
