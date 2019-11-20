89f410881034e0e00e2fbac1a63ff58b65c23a45
lurkor <lurkortv@gmail.com>
2019-11-19 21:24:00 -0800

    Finished coding, tests, summary comments and blog.

---
+++ Challenges/MergeSort/Blog.md
@@ -0,0 +1,77 @@
+# Insertion Sort
+---
+
+Merge sort is a method of sorting an array.  Merge sort sorts an array by splitting the array in half recursively until there are only
+single numbers left in the array, and then combining the arrays as we go back through the call stack.
+
+---
+
+## Pseudo Code
+
+```
+ALGORITHM Mergesort(arr)
+    DECLARE n <-- arr.length
+           
+    if n > 1
+      DECLARE mid <-- n/2
+      DECLARE left <-- arr[0...mid]
+      DECLARE right <-- arr[mid...n]
+      // sort the left side
+      Mergesort(left)
+      // sort the right side
+      Mergesort(right)
+      // merge the sorted left and right sides together
+      Merge(left, right, arr)
+
+ALGORITHM Merge(left, right, arr)
+    DECLARE i <-- 0
+    DECLARE j <-- 0
+    DECLARE k <-- 0
+
+    while i < left.length && j < right.length
+        if left[i] <= right[j]
+            arr[k] <-- left[i]
+            i <-- i + 1
+        else
+            arr[k] <-- right[j]
+            j <-- j + 1
+            
+        k <-- k + 1
+
+    if i = left.length
+       set remaining entries in arr to remaining values in right
+    else
+       set remaining entries in arr to remaining values in left
+
+
+
+```
+
+---
+
+## Explanation
+
+![Merge Sort Image 1](assets/Blog1.png)
+1) The method first sorts the input array into two arrays, a left and right array.  The array is split by defining a middle
+point which is the array length / 2.
+![Merge Sort Image 2](assets/Blog2.png)
+2) The method is called recursively, splitting the array again.
+![Merge Sort Image 3](assets/Blog3.png)
+3) The recursive calls stop when all arrays have a length of 1.
+![Merge Sort Image 4](assets/Blog4.png)
+4) Once the length of the array is one, the recursive functions start popping off the stack. The function merges the left and 
+right arrays together, sorting them as they go.
+
+---
+
+# Efficiency
+
+### Time: 
+* Best: O(nlg(n))
+* Worst: O(nlg(n))
+  * The function runs consistently every time going through each variable, so the O of time is based on the number of recursive
+  calls it takes to break the left and right down to be length of 1.
+### Space:
+* Best: O(n)
+* Worst: O(n)
+  * The merge sort creates variables in space, so it creates a variable for each number, thus O(n).
--- Challenges/MergeSort/SortMerge/SortMerge/Program.cs
+++ Challenges/MergeSort/SortMerge/SortMerge/Program.cs
@@ -2,11 +2,110 @@
 
 namespace SortMerge
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(String.Join(',', MergeSort(new int[] { 8, 4, 23, 42, 16, 15 })));
+            Console.WriteLine(String.Join(',', MergeSort(new int[] { 5, 12, 7, 5, 5, 7 })));
+            Console.WriteLine(String.Join(',', MergeSort(new int[] { 20, 18, 12, 8, 5, -2 })));
+        }
+        /// <summary>
+        /// Takes in an array of integers and sorts it using the merge sort algorithm from lowest to highest value.
+        /// </summary>
+        /// <param name="arr">Takes in an array of integers.</param>
+        /// <returns>Returns the parameter array.</returns>
+        public static int[] MergeSort(int[] arr)
+        {
+            int arrLength = arr.Length;
+
+            if (arrLength > 1)
+            {
+                int mid = arr.Length / 2;
+                int[] left;
+                int[] right;
+
+                if (arr.Length % 2 == 0)
+                {
+                    left = new int[mid];
+                    right = new int[mid];
+                }
+                else
+                {
+                    left = new int[mid];
+                    right = new int[mid + 1];
+                }
+
+                bool half = false;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (i == mid)
+                    {
+                        half = true;
+                        right[i - mid] = arr[i];
+                    }
+                    else if (half == false)
+                    {
+                        left[i] = arr[i];
+                    }
+                    else
+                    {
+                        right[(i - mid)] = arr[i];
+                    }
+
+                }
+
+                MergeSort(left);
+                MergeSort(right);
+                Merge(left, right, arr);
+            }
+            return arr;
+        }
+        /// <summary>
+        /// Private method used in the merge sort method.  Merges the left and right arrays together back into the input array.
+        /// </summary>
+        /// <param name="left">Left half of the array.</param>
+        /// <param name="right">Right half of the array.</param>
+        /// <param name="arr">The orignal array parameter from the main function.</param>
+        private static void Merge(int[] left, int[] right, int[] arr)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+            if (i == left.Length)
+            {
+                while (j < right.Length)
+                {
+                    arr[k] = right[j];
+                    j++;
+                    k++;
+                }
+            }
+            else
+            {
+                while (i < left.Length)
+                {
+                    arr[k] = left[i];
+                    i++;
+                    k++;
+                }
+            }
         }
     }
 }
