fecef573741d3c8c2adc3abe08b7bb274185a3cd
cdcummings10 <ccummings0913@gmail.com>
2019-11-19 19:31:27 -0800

    Committing scaffold

---
+++ Challenges/MergeSort/SortMerge/SortMerge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
