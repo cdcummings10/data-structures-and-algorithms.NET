775b6bf6055f402074eeaa617092bcf0a03e155e
cdcummings10 <ccummings0913@gmail.com>
2019-11-25 08:27:19 -0800

    Finished readme and whiteboard for challenge.

---
+++ DataStructures/HashTable/HashMap/HashMap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Enum.Parse(typeof(int), "Hello"));
+        }
+    }
+}
